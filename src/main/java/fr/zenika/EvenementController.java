439ad03dd483fe0e93d64bd5589a66c0cabd23eb
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2020-11-19 18:54:45 +0100

    Get and add, with unit test

--- src/main/java/fr/zenika/EvenementController.java
+++ src/main/java/fr/zenika/EvenementController.java
@@ -3,11 +3,11 @@ package fr.zenika;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Path("/v1/evenements")
@@ -22,11 +22,15 @@ public class EvenementController {
     }
 
     @GET
-    @Path("/init")
+    @Path("/{id}")
+    public Evenement getEvenement(@PathParam("id") Long id){
+        return Evenement.findById(id);
+    }
+
+    @POST
     @Transactional
-    public void init(){
-        var evenement = new Evenement("Devfest Nantes", LocalDate.of(2021, Month.OCTOBER, 21), 2);
+    public Evenement add(Evenement evenement){
         evenement.persist();
-        new Evenement("Devoxx", LocalDate.of(2021, Month.APRIL, 15), 3).persist();
+        return evenement;
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,8 @@
 insert into
     Evenement
-    (name, debut, duree)
+    (id, nom, debut, duree)
 values
-    ('DevFest Nantes', '2021-10-21', 2),
-    ('Devoxx France', '2021-05-07', 3),
-    ('BattleDev', '2020-11-26', 1);
+    (1, 'DevFest Nantes', '2021-10-21', 2),
+    (2, 'Devoxx France', '2021-05-07', 3),
+    (3, 'BattleDev', '2020-11-26', 1);
+alter sequence hibernate_sequence restart with 5;
